59c5e38b77be2bb4a4cde7960d6d3d7b97e176e6
BJ Hayes <b.justinhayes@gmail.com>
2018-06-11 18:27:50 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,62 @@
+'use strict';
+
+// variable to store an array of store hours for all stores
+
+var StoreHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
+
+//an array of store names
+
+var allOfTheStores = [firstandPike, seaTac, seattleCenter, capitolHill, alki]
+
+// a list of object literals that represent each store
+
+var firstandPike = {
+  minCust: 23,
+  maxCust: 65,
+  avgCookiesSold: 6.3,
+  getRandomInt: function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+  },
+};
+// I couldn't get the "cookietest" function to work inside of the object, even when I used "this.getRandomInt" but it works outside of the firstandPike object so that's why it's there
+var firstandPikeSaleSim = firstandPike.getRandomInt(firstandPike.minCust,firstandPike.maxCust)
+  console.log(firstandPikeSaleSim)
+
+
+
+var seaTac = {
+  minCust: 3,
+  maxCust: 24,
+  avgCookiesSold: 1.2,
+  getRandomInt: function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+  },
+   
+};
+
+var seattleCenter = {
+  minCust: 11,
+  maxCust: 38,
+  avgCookiesSold: 3.7,
+  getRandomInt: function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+  },
+};
+
+var capitolHill = {
+  minCust: 20,
+  maxCust: 38,
+  avgCookiesSold: 2.3,
+  getRandomInt: function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+  },
+};
+
+var alki = {
+  minCust: 2,
+  maxCust: 16,
+  avgCookiesSold: 4.6,
+  getRandomInt: function(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min
+  },
+};
