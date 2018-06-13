bea727d9c8b39814a0da8ea7d3dea09b4baef46c
BJ Hayes <b.justinhayes@gmail.com>
2018-06-13 00:11:17 -0700

    created instances of the hotels using constructor function

--- js/app.js
+++ js/app.js
@@ -8,87 +8,112 @@ var StoreHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2p
 
 var allOfTheStores = [firstandPike, seaTac, seattleCenter, capitolHill, alki];
 
-// a list of object literals that represent each store
+// A Constructor Function for Salmon Cookies Stores
 
-var firstandPike = {
-  minCust: 23,
-  maxCust: 65,
-  avgCookiesSold: 6.3,
-  firstandPikeSaleSim: function() {
+function SalmonCookieStores (name, storeHours, minCust, maxCust, avgCookiesSold) {
+  this.name = name;
+  this.storeHours = allOfTheStores;
+  this.minCust: minCust;
+  this.maxCust: maxCust;
+  this.avgCookiesSold: avgCookiesSold;
+  this.salesSim = function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-  },
-  averagePerHour: function() {
+  };
+  this.averagePerHour = function() {
     return this.firstandPikeSaleSim() * this.avgCookiesSold;
-  },
+  };
+}
 
-  generateList: function() {
-    var ulEl = document.getElementById('firstandPike');
-    var runningTotal = 0;
+var firstandPike = new SalmonCookieStores ('firstandPike', storeHours, 23, 65, 6.3);
+var seaTac = new SalmonCookieStores ('seaTac', storeHours, 3, 24, 1.2,);
+var seattleCenter = SalmonCookieStores ('seattleCenter', storeHours, 11, 38, 3.7);
+var capitolHill =  SalmonCookieStores ('capitolHill', storeHours, 20, 38, 2.3);
+var alki = SalmonCookieStores ('alki', storeHours, 2, 16, 4.6);
 
-    for(var i = 0; i < StoreHours.length; i++) {
+// // SalmonCookieStores.name = [firstandPike, seaTac, seattleCenter, capitolHill, alki];
 
-      // this creates the list elements
-      var liEl = document.createElement('li');
 
-      // this populates the list 
-      liEl.textContent = this.averagePerHour();
+// // a list of object literals that represent each store
+
+// var firstandPike = {
+//   minCust: 23,
+//   maxCust: 65,
+//   avgCookiesSold: 6.3,
+//   firstandPikeSaleSim: function() {
+//     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+//   },
+//   averagePerHour: function() {
+//     return this.firstandPikeSaleSim() * this.avgCookiesSold;
+//   },
+
+//   generateList: function() {
+//     var ulEl = document.getElementById('firstandPike');
+//     var runningTotal = 0;
+
+//     for(var i = 0; i < StoreHours.length; i++) {
+
+//       // this creates the list elements
+//       var liEl = document.createElement('li');
+
+//       // this populates the list 
+//       liEl.textContent = this.averagePerHour();
       
-      // append it to the DOM
-      ulEl.appendChild(liEl);
-    }
-  },
-};
+//       // append it to the DOM
+//       ulEl.appendChild(liEl);
+//     }
+//   },
+// };
 
 
-/* I couldn't get the "cookietest" function to work inside of the object, even when I used "this.getRandomInt" but it works outside of the firstandPike object so that's why it's there
+// /* I couldn't get the "cookietest" function to work inside of the object, even when I used "this.getRandomInt" but it works outside of the firstandPike object so that's why it's there
 
-var firstandPikeSaleSim = firstandPike.getRandomInt(firstandPike.minCust,firstandPike.maxCust);
+// var firstandPikeSaleSim = firstandPike.getRandomInt(firstandPike.minCust,firstandPike.maxCust);
 
-console.log(firstandPikeSaleSim);
+// console.log(firstandPikeSaleSim);
 
- console.log(firstandPikeSaleSim) proof it worked */
+//  console.log(firstandPikeSaleSim) proof it worked */
 
-var seaTac = {
-  minCust: 3,
-  maxCust: 24,
-  avgCookiesSold: 1.2,
-  getRandomInt: function(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  },
-};
+// var seaTac = {
+//   minCust: 3,
+//   maxCust: 24,
+//   avgCookiesSold: 1.2,
+//   getRandomInt: function(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+//   },
+// };
 
-var seaTacSaleSim = seaTac.getRandomInt(seaTac.minCust, seaTac.maxCust);
+// var seaTacSaleSim = seaTac.getRandomInt(seaTac.minCust, seaTac.maxCust);
 
-var seattleCenter = {
-  minCust: 11,
-  maxCust: 38,
-  avgCookiesSold: 3.7,
-  getRandomInt: function(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  },
-};
+// var seattleCenter = {
+//   minCust: 11,
+//   maxCust: 38,
+//   avgCookiesSold: 3.7,
+//   getRandomInt: function(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+//   },
+// };
 
-var seattleCentersalesSim = seattleCenter.getRandomInt(seattleCenter.minCust, seattleCenter.maxCust);
+// var seattleCentersalesSim = seattleCenter.getRandomInt(seattleCenter.minCust, seattleCenter.maxCust);
 
-var capitolHill = {
-  minCust: 20,
-  maxCust: 38,
-  avgCookiesSold: 2.3,
-  getRandomInt: function(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  },
-};
+// var capitolHill = {
+//   minCust: 20,
+//   maxCust: 38,
+//   avgCookiesSold: 2.3,
+//   getRandomInt: function(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+//   },
+// };
 
-var capitolHillsalesSim = capitolHill.getRandomInt(capitolHill.minCust, capitolHill.maxCust);
+// var capitolHillsalesSim = capitolHill.getRandomInt(capitolHill.minCust, capitolHill.maxCust);
 
 
-var alki = {
-  minCust: 2,
-  maxCust: 16,
-  avgCookiesSold: 4.6,
-  getRandomInt: function(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  },
-};
+// var alki = {
+//   minCust: 2,
+//   maxCust: 16,
+//   avgCookiesSold: 4.6,
+//   getRandomInt: function(min, max) {
+//     return Math.floor(Math.random() * (max - min)) + min;
+//   },
+// };
 
-var alkiSalesSim = alki.getRandomInt(alki.minCust, alki.maxCust);
+// var alkiSalesSim = alki.getRandomInt(alki.minCust, alki.maxCust);
