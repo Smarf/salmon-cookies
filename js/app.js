ea0e3ad9c8c807754e9cffbd9a7740e2b1642ea0
BJ Hayes <b.justinhayes@gmail.com>
2018-06-15 12:02:13 -0700

    cleaned up code spacing, added the working math to each store object, verified that customerPerHour math works

--- js/app.js
+++ js/app.js
@@ -4,101 +4,102 @@
 
 var storeHours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
-//an array of store names
-
 // var allOfTheStores = [firstandPike, seaTac, seattleCenter, capitolHill, alki];
 
 // a list of object literals that represent each store
+// note that storeObject.customersPerHour generates an array which simulates cookie sold *per hour* 
 
 var firstandPike = {
   minCust: 23,
   maxCust: 65,
   avgCookiesSold: 6.3,
   customersPerHour: [],
-  firstandPikeSaleSim: function() {
+  salesSimulator: function() {
     for (var i = 0; i < storeHours.length; i++) {
       this.customersPerHour.push(Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust));
     }
   },
   averagePerHour: function() {
-    return this.firstandPikeSaleSim() * this.avgCookiesSold;
+    return this.salesSimulator() * this.avgCookiesSold;
   },
 };
 
-firstandPike.firstandPikeSaleSim();
+var seaTac = {
+    minCust: 3,
+    maxCust: 24,
+    avgCookiesSold: 1.2,
+    customersPerHour: [],
+  salesSimulator: function() {
+    for (var i = 0; i < storeHours.length; i++) {
+      this.customersPerHour.push(Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust));
+    }
+  },
+  averagePerHour: function() {
+    return this.salesSimulator() * this.avgCookiesSold;
+  },
+};
+   
+var seattleCenter = {
+minCust: 11,
+maxCust: 38,
+avgCookiesSold: 3.7,
+customersPerHour: [],
+  customersPerHour: function() {
+    for (var i = 0; i < storeHours.length; i++) {
+      this.customersPerHour.push(Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust));
+    }
+  },
+  averagePerHour: function() {
+    return this.customersPerHour() * this.avgCookiesSold;
+  },
+};
+        
+var capitolHill = {
+minCust: 20,
+maxCust: 38,
+avgCookiesSold: 2.3,
+customersPerHour: [],
+  customersPerHour: function() {
+    for (var i = 0; i < storeHours.length; i++) {
+      this.customersPerHour.push(Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust));
+    }
+  },
+  averagePerHour: function() {
+    return this.customersPerHour() * this.avgCookiesSold;
+  },
+},
 
-console.log(firstandPike.customersPerHour);
+var alki = {
+minCust: 2,
+maxCust: 16,
+avgCookiesSold: 4.6,
+customersPerHour: [],
+  customersPerHour: function() {
+    for (var i = 0; i < storeHours.length; i++) {
+      this.customersPerHour.push(Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust));
+    }
+  },
+  averagePerHour: function() {
+    return this.customersPerHour() * this.avgCookiesSold;
+  },
+};
 
-/* to complete this, you're going to have to use the append.child function to update the DOM, which won't change your HTML but we'll get to that tomorrow */
-
-// var seaTac = {
-//   minCust: 3,
-//   maxCust: 24,
-//   avgCookiesSold: 1.2,
-//   getRandomInt: function(min, max) {
-//     return Math.floor(Math.random() * (max - min)) + min;
-//   },
-// };
-
-// var seaTacSaleSim = seaTac.getRandomInt(seaTac.minCust, seaTac.maxCust);
-
-// var seattleCenter = {
-//   minCust: 11,
-//   maxCust: 38,
-//   avgCookiesSold: 3.7,
-//   getRandomInt: function(min, max) {
-//     return Math.floor(Math.random() * (max - min)) + min;
-//   },
-// };
-
-// var seattleCentersalesSim = seattleCenter.getRandomInt(seattleCenter.minCust, seattleCenter.maxCust);
-
-// var capitolHill = {
-//   minCust: 20,
-//   maxCust: 38,
-//   avgCookiesSold: 2.3,
-//   getRandomInt: function(min, max) {
-//     return Math.floor(Math.random() * (max - min)) + min;
-//   },
-// };
-
-// var capitolHillsalesSim = capitolHill.getRandomInt(capitolHill.minCust, capitolHill.maxCust);
-
-
-// var alki = {
-//   minCust: 2,
-//   maxCust: 16,
-//   avgCookiesSold: 4.6,
-//   getRandomInt: function(min, max) {
-//     return Math.floor(Math.random() * (max - min)) + min;
-//   },
-// };
-
-// var alkiSalesSim = alki.getRandomInt(alki.minCust, alki.maxCust);
-
-//   generateList: function() {
-//     var ulEl = document.getElementById('firstandPike');
-//     var runningTotal = 0;
-
-//     for(var i = 0; i < StoreHours.length; i++) {
-
-//       // this creates the list elements
-//       var liEl = document.createElement('li');
-
-//       // this populates the list
-//       liEl.textContent = this.averagePerHour();
-
-//       // append it to the DOM
-//       ulEl.appendChild(liEl);
-//     }
-//   },
-// };
-
-
-/* I couldn't get the "cookietest" function to work inside of the object, even when I used "this.getRandomInt" but it works outside of the firstandPike object so that's why it's there
-
-var firstandPikeSaleSim = firstandPike.getRandomInt(firstandPike.minCust,firstandPike.maxCust);
-
-console.log(firstandPikeSaleSim);
-
- console.log(firstandPikeSaleSim) proof it worked */
+                  //   generateList: function() {
+                  //     var ulEl = document.getElementById('firstandPike');
+                  //     var runningTotal = 0;
+                  
+                  //     for(var i = 0; i < StoreHours.length; i++) {
+                    
+                    //       // this creates the list elements
+                    //       var liEl = document.createElement('li');
+                    
+                    //       // this populates the list
+                    //       liEl.textContent = this.averagePerHour();
+                    
+                    //       // append it to the DOM
+                    //       ulEl.appendChild(liEl);
+                    //     }
+                    //   },
+                    // };
+                    
+                    /* to complete this, you're going to have to use the append.child function to update the DOM, which won't change your HTML but we'll get to that tomorrow */
